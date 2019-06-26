078d9642e2813e8e8b179e524c56cb87a37fac6b
JamesChen <james_chen@amaxgs.com>
2019-06-26 09:47:41 +0800

    修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,12 @@ git config --local user.name "lanccj"
 git config --local user.email "lanccj@163.com"
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
-git push -f git@github.com:lanccj/lanccj.github.io.git master
+# git push -f git@github.com:lanccj/lanccj.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 注意这里是加入Travis配置的推送
+git push -f https://$access_token}@github.com/lanccj/lanccj.github.io.git master
 
 cd -
